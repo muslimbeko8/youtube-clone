91b6ca71069f93e7a44bfbbf9169d3e0401f4a9c
muslimbeko8 <blakmaxmudov@gmail.com>
2025-01-03 14:49:08 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const API_KEY = 'AIzaSyAw2IJmVSvvMoiOyZn2XmHvhsccXV0Ntlc';
+
+export const value_converter = (value) => {
+    if (value >= 1000000) return Math.floor(value/1000000) + 'M';
+    else if (value >= 1000) return Math.floor(value/1000) + 'K';
+    else return value
+}
